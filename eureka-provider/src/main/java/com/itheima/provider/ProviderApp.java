e96a996b63168bd05c750f957ad7a33cc51526ec
GuoWeiALiu <zimo576583295@outlook.com>
2021-07-27 22:17:51 +0800

    实现Hytrix  服务端 降级

--- eureka-provider/src/main/java/com/itheima/provider/ProviderApp.java
+++ eureka-provider/src/main/java/com/itheima/provider/ProviderApp.java
@@ -2,14 +2,16 @@ package com.itheima.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @author guoweiliu
  * @date 2021-07-26 22:06
  */
-
-@EnableEurekaClient
+@EnableCircuitBreaker  //开启hystrix
+@EnableEurekaClient   //开Eureka
 @SpringBootApplication
 public class ProviderApp {
     public static void main(String[] args) {
--- eureka-provider/src/main/java/com/itheima/provider/controller/GoodsController.java
+++ eureka-provider/src/main/java/com/itheima/provider/controller/GoodsController.java
@@ -2,6 +2,8 @@ package com.itheima.provider.controller;
 
 import com.itheima.provider.domain.Goods;
 import com.itheima.provider.service.GoodsService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,17 +22,40 @@ public class GoodsController {
     private GoodsService goodsService;
     @Value("${server.port}")
     private int port;
+/*
+* 降价：
+* 1 出现一场
+* 2 服务调用超时
+* */
+    @HystrixCommand(fallbackMethod = "findOne_fallback",
+            commandProperties = {
+            //设置hystrix 超时时间
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "9000")})
     @GetMapping("/findOne/{id}")
-    public Goods findOne(@PathVariable("id") int id){
+    public Goods findOne(@PathVariable("id") int id) {
         Goods goods = goodsService.findOne(id);
+        //int i = 3/0;
         try {
-            Thread.sleep(1000);
+            Thread.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        goods.setTitle(goods.getTitle()+":"+port);  //端口号设置到商品标题
+        goods.setTitle(goods.getTitle() + ":" + port);  //端口号设置到商品标题
         return goods;
     }
 
+
+    /*
+    * 定义降级方法：
+    * 1 方法返回值和原方法一样
+    * 2 方法的参数和原方法一样
+    *
+    * */
+    public Goods findOne_fallback(@PathVariable("id") int id) {
+        Goods goods = new Goods();
+        goods.setTitle("降级处理");
+
+        return goods;
+    }
 }
 
